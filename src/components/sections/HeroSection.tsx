3d2757d8d36ed19c1675215417f308da1e3430d5
Saiprasath R S <65589605+seirralphaind@users.noreply.github.com>
2026-02-02 14:22:37 +0530

    Remove scroll indicator from HeroSection
    Removed the scroll indicator from the HeroSection component.

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -70,11 +70,6 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-
-      {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-        <ArrowDown className="h-6 w-6 text-muted-foreground" />
-      </div>
     </section>
   );
 };
